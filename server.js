8bd136405540b738d240a58aeef304e351039bbd
Yaroslav483 <yaroslav.niko2506@gmail.com>
2026-05-28 18:43:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const express = require('express');
 
 const app = express();
 
-// корень проекта (files)
+
 const root = __dirname;
 
-// статика (css, js, assets, html)
+
 app.use(express.static(root));
 
-// главная страница
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(root, 'html', 'index.html'));
 });
 
-// каталог
+
 app.get('/catalog', (req, res) => {
   res.sendFile(path.join(root, 'html', 'catalog.html'));
 });
 
-// продукт
+
 app.get('/product', (req, res) => {
   res.sendFile(path.join(root, 'html', 'product.html'));
 });
